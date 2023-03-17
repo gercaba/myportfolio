2322cfaa0ca54e750874b75701a4abc067de5f9d
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-03-17 20:42:48 +0100

    init lenguage

--- src/Componentes/Projects.jsx
+++ src/Componentes/Projects.jsx
@@ -29,6 +29,8 @@ export const Projects = () =>{
             <a
               href={project.link}
               key={project.image}
+              target="_blank"
+              rel="noreferrer"
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
                 <img
